d58d9c9848df290b9507250d4ccf9b64fd1bd4a6
Abhilash <bhattacharjeev@gmail.com>
2016-10-20 01:17:52 +0530

    Segregating

--- js/main.js
+++ js/main.js
@@ -126,26 +126,6 @@ $('.menu-item').on( 'click', function() {
 	$('input, textarea').placeholder();
 
 	 
-        $('#myCarousel').carousel({
-                interval: 2000
-        });
- 
-        //Handles the carousel thumbnails
-        $('[id^=carousel-selector-]').click(function () {
-        var id_selector = $(this).attr("id");
-        try {
-            var id = /-(\d+)$/.exec(id_selector)[1];
-            console.log(id_selector, id);
-            jQuery('#myCarousel').carousel(parseInt(id));
-        } catch (e) {
-            console.log('Regex failed!', e);
-        }
-    });
-        // When the carousel slides, auto update the text
-        $('#myCarousel').on('slid.bs.carousel', function (e) {
-                 var id = $('.item.active').data('slide-number');
-                $('#carousel-text').html($('#slide-content-'+id).html());
-        });
 
 
       
