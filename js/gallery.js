7f8e41ad502cde59295c8f7298fba9dcabdab4c4
Abhilash <bhattacharjeev@gmail.com>
2016-10-20 01:35:26 +0530

    Carousal script updated.

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,17 @@
- $('#myCarousel').carousel({
+    $('#myCarousel').carousel({
                 interval: 5000
         });
  
+        $('#carousel-text').html($('#slide-content-0').html());
+ 
         //Handles the carousel thumbnails
-        $('[id^=carousel-selector-]').click(function () {
-        var id_selector = $(this).attr("id");
-        try {
-            var id = /-(\d+)$/.exec(id_selector)[1];
-            console.log(id_selector, id);
-            jQuery('#myCarousel').carousel(parseInt(id));
-        } catch (e) {
-            console.log('Regex failed!', e);
-        }
-    });
+       $('[id^=carousel-selector-]').click( function(){
+            var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+            var id = parseInt(id);
+            $('#myCarousel').carousel(id);
+        });
+ 
+ 
         // When the carousel slides, auto update the text
         $('#myCarousel').on('slid.bs.carousel', function (e) {
                  var id = $('.item.active').data('slide-number');
